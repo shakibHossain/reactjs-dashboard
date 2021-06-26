b92809e81840c431a69a09d545a6b85c1997450b
shakibHossain <shakib.hossain.sh@gmail.com>
2021-06-26 08:50:20 -0600

    add react-router-dom & login view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import HomePage from "./pages/home/home.page";
+import LoginPage from "./pages/login/login.page";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
