cea2e3908fe9d95bd968f65e957b2e9940d975bc
shakibHossain <shakib.hossain.sh@gmail.com>
2021-07-03 10:15:48 -0600

    add table with dummy data on view employee page

--- src/pages/view-employees/view-employees.page.jsx
+++ src/pages/view-employees/view-employees.page.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 import Menu from "../../components/menu/menu.component";
 
@@ -31,7 +37,24 @@ const useStyles = makeStyles((theme) => ({
     position: "fixed",
     bottom: 0,
   },
+  table: {
+    minWidth: 650,
+  },
+  pageheading: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
 }));
+
+function createData(firstName, lastName, email) {
+  return { firstName, lastName, email };
+}
+
+const rows = [
+  createData("test1", "test2", "test12@test.com"),
+  createData("test3", "test4", "test34@test.com"),
+  createData("test5", "test6", "test56@test.com"),
+];
 
 const ViewEmployeesPage = () => {
   const classes = useStyles();
@@ -43,10 +66,33 @@ const ViewEmployeesPage = () => {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <h2>View Employees</h2>
-          <Box pt={4} className={classes.footer}>
-            {/* <Copyright /> */}
-          </Box>
+          <Typography
+            component="h1"
+            variant="h5"
+            className={classes.pageheading}
+          >
+            View Employees
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Email</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.email}>
+                    <TableCell>{row.firstName}</TableCell>
+                    <TableCell>{row.lastName}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Container>
       </main>
     </div>
