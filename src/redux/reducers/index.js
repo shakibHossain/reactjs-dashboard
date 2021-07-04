a8d985ad13a8ba1bb555283cbe984dfc37cd7fc6
shakibHossain <shakib.hossain.sh@gmail.com>
2021-07-04 07:15:25 -0600

    update auth.reducers filename to auth.reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import authReducer from "./auth.reducers";
+import authReducer from "./auth.reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
